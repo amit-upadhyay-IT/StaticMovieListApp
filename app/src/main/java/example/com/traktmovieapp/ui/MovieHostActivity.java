9e3531f6ee87ded8bfc6fc831e61783472cb56ee
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-07-28 22:25:59 +0530

    First Commit

---
+++ app/src/main/java/example/com/traktmovieapp/ui/MovieHostActivity.java
@@ -0,0 +1,24 @@
+package example.com.traktmovieapp.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import example.com.traktmovieapp.R;
+
+public class MovieHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_host);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(android.R.drawable.btn_star);
+        setSupportActionBar(toolbar);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.containerLayout, new MovieFragment())
+                .commit();
+    }
+}
